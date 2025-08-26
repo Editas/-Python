7edfbfbf5d61af727874b494e6f3bcffb509984a
Edita <50019566+Editas@users.noreply.github.com>
2025-08-26 12:45:52 +0300

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from src.app.main import app
+
+
+client = TestClient(app)
+
+
+def test_root_ok():
+resp = client.get("/")
+assert resp.status_code == 200
+assert resp.json() == {"status": "ok"}
